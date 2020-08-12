875aab3ba2ee33302d58f3c6e0eb6778b17f3e1a
Jan Gevaert <jan.gevaert@gmail.com>
2020-08-12 11:16:14 -0600

    Initial commit

---
+++ src/GalToLitTable.java
@@ -0,0 +1,19 @@
+public class GalToLitTable {
+
+  public static void main(String[] args) {
+    double gallons, liters;
+    int count = 0;
+
+    for (gallons = 1; gallons <= 100; gallons++) {
+      liters = gallons * 3.7854;
+      System.out.println(gallons + " gallons is " + liters + " liters.");
+    count++;
+    if (count == 10) {
+      System.out.println();
+      count = 0;
+    }
+
+    }
+
+  }
+}
